2fbe99652d8cabf53a30a79782c999d0120c5cca
Jaspreet Singh <jaspreetsinghtuli@gmail.com>
2021-06-25 22:02:19 +0530

    added quizzes cntroller

---
+++ QuizMe.WS/Controllers/QuizzesController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace QuizMe.WS.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class QuizzesController : ControllerBase
+    {
+        // GET: api/<QuizzesController>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<QuizzesController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<QuizzesController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<QuizzesController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<QuizzesController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
