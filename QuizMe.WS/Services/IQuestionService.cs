ca6b5b2d15142773d0c0dc5dde5e5a2b1dd1a991
Jaspreet Singh <jaspreetsinghtuli@gmail.com>
2021-07-10 23:12:04 +0530

    added question service

---
+++ QuizMe.WS/Services/IQuestionService.cs
@@ -0,0 +1,16 @@
+﻿using QuizMe.WS.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace QuizMe.WS.Services
+{
+    public interface IQuestionService
+    {
+        public bool AddQuestion(Question question);
+        public IEnumerable<Question> GetQuestions();
+        public bool EditQuestion(Question question);
+        public bool RemoveQuestion(int questionId);
+    }
+}
