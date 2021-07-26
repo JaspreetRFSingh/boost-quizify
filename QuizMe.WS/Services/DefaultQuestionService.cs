3a64f332064a4a6ecee2ceb90f6924b5f618340b
Jaspreet Singh <jaspreetsinghtuli@gmail.com>
2021-07-26 07:18:08 +0530

    Added default question service

---
+++ QuizMe.WS/Services/DefaultQuestionService.cs
@@ -0,0 +1,31 @@
+﻿using QuizMe.WS.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace QuizMe.WS.Services
+{
+    public class DefaultQuestionService : IQuestionService
+    {
+        public bool AddQuestion(Question question)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool EditQuestion(Question question)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Question> GetQuestions()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool RemoveQuestion(int questionId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
